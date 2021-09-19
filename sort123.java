f413859bfbdb0436d47cdc0f5e15633394992b6d
shivrastogi <shivrastogi01@gmail.com>
2021-09-19 08:02:49 +0530

    Sorting in a specific manner

---
+++ sort123.java
@@ -0,0 +1,22 @@
+class sort123{
+	public static void main(String [] args){
+int [] m={1,3,-2,-1,5};
+int k=0;
+int [] n=new int[m.length];
+for(int i=0; i<m.length;i++){
+	if(m[i]<0){
+	n[k]=m[i];
+	k++;}
+	else
+	System.out.print(m[i]);
+}
+
+for(int i=0;i<k;i++){
+	
+	System.out.print(n[i]);
+}
+
+	}
+
+
+}
