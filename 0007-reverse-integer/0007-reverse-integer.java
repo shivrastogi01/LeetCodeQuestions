949905d78b56c8d92a88f947f53c41ce40562ee4
shivrastogi01 <76581550+shivrastogi01@users.noreply.github.com>
2022-10-20 00:10:11 +0530

    Time: 2 ms (83.11%), Space: 40.4 MB (85.23%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -1,15 +1,15 @@
 class Solution {
     public int reverse(int x) {
-        long num = 0; // we will take long so that if a number on reverse is larger than the range of int , we can check it after wards 
-        while ( x != 0 ){ // this is to reverse the integer 
+        long num = 0; 
+        while ( x != 0 ){ // 
             int remainder = x%10;
             x = x/10;
             num = ( num*10 ) + remainder ;
         }
-        if ( num < -2147483648 || num > 2147483647 ) return 0 ; // here we check if the reverse number exceeds the range of int we will return 0
+        if ( num < -2147483648 || num > 2147483647 ) return 0 ;
         
-        int ans = (int) num; // we will convert the long to int 
-        if ( x < 0 ){ // check if the number is negatie so return with negative sign 
+        int ans = (int) num; 
+        if ( x < 0 ){ 
             return -ans; 
         }
         return ans ;
